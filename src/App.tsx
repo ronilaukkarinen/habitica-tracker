27e4b378e5d08ccbb29ebb89ed51209236ac1aa5
Roni Laukkarinen <roni@dude.fi>
2023-11-17 23:30:57 +0200

    Allow using url params

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,25 @@ function App() {
     setAppState(AppState.USER_INPUT_ACCEPTED);
   };
 
+  // Start app right away with user ID and API key from URL params
+  const urlUserId = window.location.search.split("?userId=")[1];
+  const realurlUserId = urlUserId.split("&userapiKey=")[0];
+  const urlUserApiKey = window.location.search.split("&userapiKey=")[1];
+
+  // Fill form fields one by one from url params
+  if (urlUserId && realurlUserId !== userId) {
+    setUserId(realurlUserId);
+  }
+
+  if (urlUserApiKey && urlUserApiKey !== userApiKey) {
+    setUserApiKey(urlUserApiKey);
+  }
+
+  // If both fields are filled, submit form
+  if (urlUserId && urlUserApiKey && appState === AppState.PROMPT_FOR_USER_CREDS) {
+    setAppState(AppState.USER_INPUT_ACCEPTED);
+  }
+
   if (
     appState === AppState.PROMPT_FOR_USER_CREDS ||
     appState === AppState.ERROR
